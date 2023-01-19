520179b32146eefc024804fb05ad582957c16ee5
Lorenzo Bertolini <lorenzo@vento.io>
2023-01-19 17:06:12 +0100

    setup first barchart version

---
+++ src/BarChart/Bars.tsx
@@ -0,0 +1,156 @@
+import React from 'react';
+import {
+	interpolate,
+	useCurrentFrame,
+	Sequence,
+	spring,
+	useVideoConfig,
+} from 'remotion';
+import numeral from 'numeral';
+import {scaleLinear} from '@visx/scale';
+import {COLOR_1, FONT_FAMILY} from './constants';
+
+type Area = {
+	x1: number;
+	// x2: number;
+	y1: number;
+	// y2: number;
+	width: number;
+	height: number;
+};
+
+const barsArea = {
+	x1: 700,
+	// x2: 220,
+	width: 500,
+	y1: 300,
+	// y2: 120,
+	// height: 700,
+	height: 400,
+};
+const barsAreaCSS: React.CSSProperties = {
+	position: 'absolute',
+	top: barsArea.y1,
+	left: barsArea.x1,
+	width: barsArea.width,
+	height: barsArea.height,
+};
+
+export const Bars: React.FC = () => {
+	const value = 82.5;
+
+	const frame = useCurrentFrame();
+	const opacity = interpolate(frame, [0, 10], [0, 1], {
+		extrapolateRight: 'clamp',
+	});
+
+	const {fps, width} = useVideoConfig();
+
+	// Animate from 0 to 1 after 10 frames
+	const heightFactor = spring({
+		frame: frame - 10,
+		fps,
+		durationInFrames: 20,
+	});
+
+	const valueTextOpacity = spring({
+		frame: frame - 10,
+		fps,
+		durationInFrames: 40,
+	});
+
+	const maxBarColor = '#e0e0e0';
+	const valueBarColor = '#f05122';
+
+	const yExtent = [0, barsArea.height];
+
+	const valueScale = scaleLinear({
+		domain: [0, 100],
+		range: yExtent,
+	});
+
+	const currentDisplayValue = heightFactor * value;
+
+	const minMapped = valueScale(0);
+	const maxMapped = valueScale(100);
+
+	const bigBarHeight = maxMapped - minMapped;
+
+	const currentDisplayValueMapped = valueScale(currentDisplayValue);
+	const valueBarHeight = currentDisplayValueMapped - minMapped;
+
+	const horizontalPaddingInnerBar = 0.1;
+	const paddingLeft = barsArea.width * 0.5 * horizontalPaddingInnerBar;
+
+	return (
+		<div>
+			<div
+				style={{
+					paddingTop: 140,
+					display: 'flex',
+					justifyContent: 'center',
+					width,
+				}}
+			>
+				<div
+					style={{
+						fontFamily: FONT_FAMILY,
+						fontSize: 130,
+						fontWeight: 800,
+						opacity: valueTextOpacity,
+						// transform: `scale(${valueTextOpacity})`,
+					}}
+				>
+					{formatPercentage({amount: currentDisplayValue / 100})}
+				</div>
+			</div>
+			<div style={{...barsAreaCSS, opacity}}>
+				<div style={{position: 'relative'}}>
+					<div
+						style={{
+							top: 0,
+							width: barsArea.width,
+							backgroundColor: maxBarColor,
+							height: bigBarHeight,
+							position: 'absolute',
+						}}
+					/>
+					<Sequence from={10}>
+						<div
+							style={{
+								top: maxMapped - valueBarHeight,
+								left: paddingLeft,
+								width: barsArea.width * (1 - horizontalPaddingInnerBar),
+								backgroundColor: valueBarColor,
+								height: valueBarHeight,
+								position: 'absolute',
+							}}
+						/>
+					</Sequence>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export function formatPercentage({
+	amount,
+	decimals = 2,
+	withSign = false,
+}: {
+	amount?: number;
+	decimals?: number;
+	withSign?: boolean;
+}) {
+	if (amount === null || amount === undefined) {
+		return 'NaN';
+	}
+
+	const fmtStrings = ['0,0%', '0,0.0%', '0,0.00%', '0,0.000%', '0,0.0000%'];
+	const fmtString = fmtStrings[decimals];
+
+	const formattedAmount = numeral(amount).format(fmtString);
+	const signPrefix = withSign ? (amount > 0 ? '+' : '') : '';
+
+	return `${signPrefix}${formattedAmount}`;
+}
--- src/Root.tsx
+++ src/Root.tsx
@@ -1,8 +1,19 @@
 import {Composition} from 'remotion';
 import {HelloWorld} from './HelloWorld';
+import {BarChart} from './BarChart';
 import {Logo} from './HelloWorld/Logo';
 
 // Each <Composition> is an entry in the sidebar!
+
+const verticalVideo = {
+	height: 1920,
+	width: 1080,
+};
+
+const horizontalVideo = {
+	width: 1920,
+	height: 1080,
+};
 
 export const RemotionRoot: React.FC = () => {
 	return (
@@ -10,12 +21,33 @@ export const RemotionRoot: React.FC = () => {
 			<Composition
 				// You can take the "id" to render a video:
 				// npx remotion render src/index.ts <id> out/video.mp4
+				id="BarChart"
+				component={BarChart}
+				durationInFrames={150}
+				fps={30}
+				// width={1920}
+				// height={1080}
+				// width={verticalVideo.width}
+				// height={verticalVideo.height}
+				{...horizontalVideo}
+				// You can override these props for each render:
+				// https://www.remotion.dev/docs/parametrized-rendering
+				defaultProps={{
+					titleText: 'Welcome to Remotion',
+					titleColor: 'black',
+				}}
+			/>
+			<Composition
+				// You can take the "id" to render a video:
+				// npx remotion render src/index.ts <id> out/video.mp4
 				id="HelloWorld"
 				component={HelloWorld}
 				durationInFrames={150}
 				fps={30}
-				width={1920}
-				height={1080}
+				// width={1920}
+				// height={1080}
+				width={verticalVideo.width}
+				height={verticalVideo.height}
 				// You can override these props for each render:
 				// https://www.remotion.dev/docs/parametrized-rendering
 				defaultProps={{
